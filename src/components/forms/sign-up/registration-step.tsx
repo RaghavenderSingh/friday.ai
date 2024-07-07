a4d7250f18e96babf09fce68edd89146207b3297
Raghavender Singh <92844911+RaghavenderSingh@users.noreply.github.com>
2024-07-08 00:34:10 +0530

    sign up added

---
+++ src/components/forms/sign-up/registration-step.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { useAuthContextHook } from "@/context/use-auth-context";
+import React, { useState } from "react";
+import { useFormContext } from "react-hook-form";
+import TypeSelectionForm from "./type-selection-form";
+
+type Props = {};
+
+const RegistrationFormStep = (props: Props) => {
+  const {
+    register,
+    formState: { errors },
+    setValue,
+  } = useFormContext();
+  const { currentStep } = useAuthContextHook();
+  const [onOTP, setOnOTP] = useState<string>("");
+  const [onUserType, setOnUserType] = useState<"owner" | "student">("owner");
+  setValue("otp", onOTP);
+
+  switch (currentStep) {
+    case 1:
+      return (
+        <TypeSelectionForm
+          register={register}
+          userType={onUserType}
+          setUserType={setOnUserType}
+        />
+      );
+    case 2:
+    //   return <DetailForm errors={errors} register={register} />;
+    case 3:
+    //   return <OTPForm onOTP={onOTP} setOTP={setOnOTP} />;
+  }
+  return <div>registration-step</div>;
+};
+
+export default RegistrationFormStep;
